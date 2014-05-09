e7cc9ba89723d0822e6db340b0989bc6c863ef94
Falaleev Maxim <max@studio107.ru>
2014-05-09 10:57:40 +0400

    Add composer

---
+++ src/Mindy/Utils/RenderTrait.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ *
+ *
+ * All rights reserved.
+ *
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 09/04/14.04.2014 16:26
+ */
+
+namespace Mindy\Utils;
+
+
+use Exception;
+use Yii;
+
+trait RenderTrait
+{
+    public function render($view, array $data = [])
+    {
+        if ($this->beforeRender($view)) {
+            $template = $this->getViewFile($view);
+            if ($template === null) {
+                throw new Exception("Template not found: $view. Search paths:\n" . implode("\n", Yii::app()->finder->getPaths()));
+            }
+
+            $output = Yii::app()->viewRenderer->render($template, $data);
+            $output = Yii::app()->middleware->processView($output);
+            $this->afterRender($view, $output);
+
+            return $output;
+        }
+
+        return null;
+    }
+
+    /**
+     * This method is invoked at the beginning of {@link render()}.
+     * You may override this method to do some preprocessing when rendering a view.
+     * @param string $view the view to be rendered
+     * @return boolean whether the view should be rendered.
+     * @since 1.1.5
+     */
+    protected function beforeRender($view)
+    {
+        return true;
+    }
+
+    /**
+     * This method is invoked after the specified is rendered by calling {@link render()}.
+     * Note that this method is invoked BEFORE {@link processOutput()}.
+     * You may override this method to do some postprocessing for the view rendering.
+     * @param string $view the view that has been rendered
+     * @param string $output the rendering result of the view. Note that this parameter is passed
+     * as a reference. That means you can modify it within this method.
+     * @since 1.1.5
+     */
+    protected function afterRender($view, &$output)
+    {
+    }
+
+    /**
+     * Returns the view script file according to the specified view name.
+     * This method must be implemented by child classes.
+     * @param string $viewName view name
+     * @return string the file path for the named view. False if the view cannot be found.
+     */
+    public function getViewFile($viewName)
+    {
+        return Yii::app()->finder->find($viewName);
+    }
+}
---
+++ src/Mindy/Utils/TitleBreadcrumbsTrait.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ *
+ *
+ * All rights reserved.
+ *
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 03/04/14.04.2014 12:39
+ */
+
+namespace Mindy\Utils;
+
+trait TitleBreadcrumbsTrait
+{
+    public $titleSortAsc = true;
+
+    private $_breadcrumbs = [];
+
+    private $_title = [];
+
+    /**
+     * @param $value array|string
+     * @return $this
+     */
+    public function setBreadcrumbs($value)
+    {
+        if (!is_array($value)) {
+            $value = [$value];
+        }
+
+        $this->_breadcrumbs = $value;
+        return $this;
+    }
+
+    /**
+     * @param $name
+     * @param $url
+     * @return $this
+     */
+    public function addBreadcrumb($name, $url)
+    {
+        $this->_breadcrumbs[$name] = $url;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getBreadcrumbs()
+    {
+        return $this->_breadcrumbs;
+    }
+
+    /**
+     * @param $value array|string
+     * @return $this
+     */
+    public function setTitle($value)
+    {
+        if (!is_array($value)) {
+            $value = [$value];
+        }
+
+        $this->_title = $value;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTitle()
+    {
+        $title = $this->_title;
+        if ($this->titleSortAsc) {
+            krsort($title);
+        }
+        return $title;
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function setPageTitle($value)
+    {
+        return $this->setTitle($value);
+    }
+
+    /**
+     * @return array
+     */
+    public function getPageTitle()
+    {
+        return $this->getTitle();
+    }
+}
