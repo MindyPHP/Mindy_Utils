097c4f7463659f458f84ef63890a6c2d023239d7
Falaleev Maxim <max@studio107.ru>
2014-10-31 18:00:17 +0300

    Up trait

--- src/Mindy/Utils/RenderTrait.php
+++ src/Mindy/Utils/RenderTrait.php
@@ -57,10 +57,9 @@ trait RenderTrait
      * and captures the display result if required.
      * @param string $_viewFile_ view file
      * @param array $_data_ data to be extracted and made available to the view file
-     * @param boolean $_return_ whether the rendering result should be returned as a string
      * @return string the rendering result. Null if the rendering result is not required.
      */
-    public function renderInternal($_viewFile_, $_data_ = null, $_return_ = false)
+    public function renderInternal($_viewFile_, $_data_ = null)
     {
         // we use special variable names here to avoid conflict when extracting data
         if (is_array($_data_)) {
@@ -68,13 +67,10 @@ trait RenderTrait
         } else {
             $data = $_data_;
         }
-        if ($_return_) {
-            ob_start();
-            ob_implicit_flush(false);
-            require($_viewFile_);
-            return ob_get_clean();
-        } else {
-            require($_viewFile_);
-        }
+
+        ob_start();
+        ob_implicit_flush(false);
+        require($_viewFile_);
+        return ob_get_clean();
     }
 }
